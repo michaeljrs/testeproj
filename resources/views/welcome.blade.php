3f540a253ac2665e69ad10dae8a0efc53870e3c1
michaeljrs <38431821+michaeljrs@users.noreply.github.com>
2018-11-08 11:24:41 -0200

    bd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,33 @@
       <link href="{{ asset('css/gradients.css') }}" rel="stylesheet" type="text/css">
       <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
     </head>
+<?php
+$conMj =  mysqli_connect("mydb.csv5qvzrti0z.us-east-1.rds.amazonaws.com", "usuario", "#192341!dev","teste");
 
+function DB_Query($sql,$conn)
+    {
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+       
+            $result = mysqli_query($conn, $sql);
+        
+            if (mysqli_num_rows($result) > 0) {
+
+                while($row = $result->fetch_row()) {
+                    $rows[]=$row;
+                }
+                return $rows;            
+            } else {
+                return [];
+            }
+    }   
+    
+    DB_Query("insert into new_table (idnew_table,new_tablecol) select max(idnew_table)+1,max(new_tablecol)+1 from new_table",$conMj);
+ print_r(DB_Query("select * from new_table",$conMj));
+
+
+?>
     <body class="">
         <div class="wrapper">
             <div class="graphics">
